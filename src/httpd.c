1adb986d29749b94b218d9ae4f651b03e3c096a1
juliaodds18 <juliaodds18@gmail.com>
2017-10-07 19:17:01 +0000

    COMBINATION WORKS OMG THE UNDERWEAR IS SO HAPPY

--- src/httpd.c
+++ src/httpd.c
@@ -1,10 +1,14 @@
-#include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <ctype.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <poll.h>
+#include <limits.h>
+#include <errno.h>
 #include <glib.h>
 #include <glib/gprintf.h>
 
@@ -107,18 +111,44 @@ int createRequest(GString *gMessage) {
 
 
 
-int main(int argc, char *argv[] )
-{
-    int sockfd;
-    struct sockaddr_in server, client;
-    char message[1024];
-    int port; 
+
+int main(int argc, char *argv[])
+{    
+    int port, sockfd = -1, funcError, on = 1, nfds = 1, currSize, newfd = -1, i, j;
+    struct sockaddr_in server;
+    char buffer[1024];
+    struct pollfd pollfds[200];
+    int timeout = 30*1000;
+    int endServer = FALSE, shrinkArray = FALSE, closeConn = FALSE;
+
     GString *gMessage = g_string_new("");
+
 
     sscanf(argv[1], "%d", &port); 
 
     // Create and bind a TCP socket.
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockfd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+    // Print error if socket failed
+    if (sockfd < 0) {
+	fprintf(stdout, "Socket() failed\n");
+	fflush(stdout); 
+	exit(-1); 
+    }
+
+    // Allow socket descriptor to be used more than once 
+    // setsockopt sets options associated with a socket, can only be called for
+    // sockets in the AF_INET domain. 
+    // int setsockopt(int s, int level, int optname, char *optval, int optlen)
+    // s = socket descriptor, level = level for which the option is being set
+    // optname = name of a specified socket option (REUSEADDR), optval = 
+    // pointer to option data, optlen = length of option data
+    funcError = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char*)&on, sizeof(on));
+    // Handle error if setsockopt fails
+    if (funcError < 0) {
+	fprintf(stdout, "setsockopt() failed\n"); 
+	fflush(stdout); 
+	exit(-1); 
+    } 
 
     // Network functions need arguments in network byte order instead of
     // host byte order. The macros htonl, htons convert the values.
@@ -126,62 +156,168 @@ int main(int argc, char *argv[] )
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = htonl(INADDR_ANY);
     server.sin_port = htons(port);
-    bind(sockfd, (struct sockaddr *) &server, (socklen_t) sizeof(server));
+    funcError = bind(sockfd, (struct sockaddr *) &server, (socklen_t) sizeof(server));
 
+    // Handle error if bind() fails
+    if (funcError < 0) {
+	fprintf(stdout, "bind() failed\n"); 
+	fflush(stdout); 
+	exit(-1); 
+    }
     // Before the server can accept messages, it has to listen to the
     // welcome port. A backlog of one connection is allowed.
-    listen(sockfd, 1);
-
-    for (;;) {
-        // We first have to accept a TCP connection, connfd is a fresh
-        // handle dedicated to this connection.
-        socklen_t len = (socklen_t) sizeof(client);
-        int connfd = accept(sockfd, (struct sockaddr *) &client, &len);
- 	
-	// Empty the gstring before reuse
-	g_string_truncate(gMessage, 0);
-
-        // Receive from connfd, not sockfd.
-        ssize_t n = recv(connfd, message, sizeof(message) - 1, 0);
-	
-	if (n < 0) {
-            //if (errno != EWOULDBLOCK) {
-	        fprintf(stdout, "recv() failed\n");
-                fflush(stdout);
-		// Have to close the connection 
-            //}
-         }
-
-         if (n == 0) {
-             fprintf(stdout, "Connection closed by client\n");
-             fflush(stdout);
-        }
-
-        // Parse the message into Gstring
-        g_string_append_len(gMessage, message, n);	
-
-	// If the method is unknown close the connection
-	if(!createRequest(gMessage)) {
-	    // Close the connection.
-	    shutdown(connfd, SHUT_RDWR);
-	    close(connfd);
-	}
-
-        message[n] = '\0';
-        fprintf(stdout, "Received GString :\n%s\n", gMessage->str);
-        fflush(stdout);	
-	
-		
-
-	// Convert message to upper case.
-        for (int i = 0; i < n; ++i) message[i] = toupper(message[i]);
-
-        // Send the message back.
-        send(connfd, message, (size_t) n, 0);
-
-        // Close the connection.
-        shutdown(connfd, SHUT_RDWR);
-        close(connfd);
+    funcError = listen(sockfd, 1);
+    // Handle error if listen() fails
+    if (funcError < 0) {
+	fprintf(stdout, "listen() failed\n"); 
+	fflush(stdout); 
+	exit(-1); 
     }
 
+    // Initialize the pollfd structure 
+    memset(pollfds, 0, sizeof(pollfds));
+    pollfds[0].fd = sockfd; 
+    pollfds[0].events = POLLIN; 
+    // TImeout???
+    fprintf(stdout, "sockfd: %d, pollfds[0]: %d\n", sockfd, pollfds[0].fd); 
+    fflush(stdout); 
+
+
+
+    while (endServer == FALSE) {
+        fprintf(stdout, "before poll\n"); 
+	fflush(stdout); 
+
+	funcError = poll(pollfds, nfds, timeout);   
+	
+	if (funcError < 0) {
+	    fprintf(stdout, "poll() failed\n"); 
+	    fflush(stdout);
+	    break;
+	}
+	if (funcError == 0) {
+	    fprintf(stdout, "poll() timed out, exiting\n"); 
+	    fflush(stdout); 
+	    break; 
+	}
+	
+	fprintf(stdout, "going into for loop in while, poll done\n");
+	fflush(stdout); 
+	currSize = nfds; 
+	for (i = 0; i < currSize; i++) {
+	     
+	    
+	    // Loop through file descriptors, determine whether it is
+	    // the listening connection or an active connection 
+	    if (pollfds[i].revents == 0) {
+		fprintf(stdout, "this is a revent == 0\n"); 
+		fflush(stdout);  
+		continue; 
+	    }
+
+	    // revents needs to be POLLIN if not 0. Else, there is an error, end the server
+	    if (pollfds[i].revents != POLLIN) {
+		fprintf(stdout, "ERROR, REVENT NOT POLLIN\n");
+		fflush(stdout);  
+		break; 
+	    } 
+
+	    fprintf(stdout, "pollfds[i].fd: %d\n", pollfds[i].fd); 
+	    if (pollfds[i].fd == sockfd) {
+		// Listening descriptor is readable
+	
+		do {
+		    fprintf(stdout, "trying to accept\n"); 
+		    fflush(stdout); 
+		    newfd = accept(sockfd, NULL, NULL);
+		    fprintf(stdout, "after accept whoo \n"); 
+		    fflush(stdout);  
+		    if (newfd < 0) {
+			if (errno != EWOULDBLOCK) { 
+			    fprintf(stdout, "accept() failed\n"); 
+			    fflush(stdout); 
+			    endServer = TRUE;
+			}
+			break;
+		    }
+
+		    // Add new connection to pollfd
+		    pollfds[nfds].fd = newfd; 
+		    pollfds[nfds].events = POLLIN; 
+		    nfds++;
+		    fprintf(stdout, "Added new thing to thing\n"); 
+		    
+		    fflush(stdout); 
+
+		} while (newfd != -1);	
+	    }
+
+	    else {
+		
+		// Existing connection is readable
+		closeConn = FALSE; 
+		fprintf(stdout, "hello from elseeee"); 
+		fflush(stdout); 
+		do {
+		    
+		    funcError = recv(pollfds[i].fd, buffer, sizeof(buffer) - 1, 0); 
+
+		    if (funcError < 0) {
+			if (errno != EWOULDBLOCK) {
+
+			    fprintf(stdout, "recv() failed\n"); 
+			    fflush(stdout); 
+			    closeConn = TRUE; 
+			}
+			break; 
+		    }
+
+		    if (funcError == 0) {
+			fprintf(stdout, "Connection closed by client\n"); 
+			fflush(stdout); 
+			break; 
+		    }
+
+		    // Parse the message into Gstring
+		    int size = funcError; 
+	            g_string_append_len(gMessage, buffer, size);	
+
+		    // If the method is unknown close the connection
+		    if(!createRequest(gMessage)) {
+	    		// Close the connection.
+	    		closeConn = TRUE; 
+		    }
+
+        	    buffer[size] = '\0';
+        	    fprintf(stdout, "Received GString :\n%s\n", gMessage->str);
+        	    fflush(stdout);	
+
+		} while (TRUE); 
+
+	    }
+	    
+	    
+	    if (closeConn) {
+		// Clean up connections that were closed
+		close(pollfds[i].fd);
+		pollfds[i].fd = -1; 
+		shrinkArray = TRUE; 
+	    }
+	}
+
+
+	if (shrinkArray) {
+
+	    for (i = 0; i < nfds; i++) {
+		if (pollfds[i].fd == -1) {
+		    for (j = i; i < nfds; j++) 
+			pollfds[j].fd = pollfds[j+1].fd; 
+		    nfds--;
+		}
+	    }
+
+	    shrinkArray = FALSE;
+	}
+       
+    }
 }
